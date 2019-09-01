54814a1c6d619d8bfc2decbf8e6122e0cdd2c6ab
Jordan Ayala <jor.ayalag@gmail.com>
2019-09-01 13:02:21 -0400

    Use glide for image loading

--- app/src/main/java/com/incursio/randomusers/users/UsersListBindings.kt
+++ app/src/main/java/com/incursio/randomusers/users/UsersListBindings.kt
@@ -3,9 +3,8 @@ package com.incursio.randomusers.users
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.incursio.randomusers.R
+import com.bumptech.glide.Glide
 import com.incursio.randomusers.repository.remote.model.User
-import com.squareup.picasso.Picasso
 
 @BindingAdapter("items")
 fun setItems(recyclerView: RecyclerView, items: List<User>) {
@@ -14,9 +13,7 @@ fun setItems(recyclerView: RecyclerView, items: List<User>) {
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, url: String) {
-    Picasso.get()
+    Glide.with(view.context)
         .load(url)
-//        .placeholder(R.drawable.user_placeholder)
-        .error(R.drawable.error_image)
         .into(view)
 }
